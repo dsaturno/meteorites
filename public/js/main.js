12bfd27d3d1e9275ec0c3be3e7fef00b99830a0b
Daniel Saturno <danielsaturno@Daniels-MacBook-Pro.local>
2016-05-04 10:05:28 -0400

    Get meteorite data from NASA using Socrata API

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,48 @@
 $(document).ready(function(){
-  var meteorites_file = '../meteorites.json';
+  var meteorites   = [],
+      m_name       = [],
+      m_mass_g     = [],
+      m_fell_found = [],
+      m_year       = [],
+      m_latitude   = [],
+      m_longitude  = [];
 
-  var m_name       = new Array();
-  var m_mass_g     = new Array();
-  var m_fell_found = new Array();
-  var m_year       = new Array();
-  var m_latitude   = new Array();
-  var m_longitude  = new Array();
+  // var token = '5Jg6AlHXMzgOUi33YHU1ixGRL';
 
   // world map
   var map = new Datamap({
-    element: document.getElementById('map-container'),
+    element: document.getElementById('basic'),
     fills: {
-      defaultFill: 'gray'
+      defaultFill: '#A8DBA8'
     },
     geographyConfig: {
-      highlightFillColor: 'lightpink'
+      highlightFillColor: 'lightblue',
+      highlightBorderColor: 'white'
     }
   }); // end map
 
-  $.getJSON(meteorites_file, function(response) {
-
-    $.each(response, function(prop, val) {
-      if (prop === "data") {
-          for (var j = 0, k = val.length; j < k; j++) {
-            m_name.push(val[j][8]);
-            m_mass_g.push(parseInt(val[j][12]));
-            m_fell_found.push(val[j][13]);
-            // won't work otherwise, even though there are no null values
-            if (val[j][14] !== null)
-              m_year.push(parseInt(val[j][14].substr(0, 4)));
-
-            m_latitude.push(val[j][15]);
-            m_longitude.push(val[j][16]);
-          }
+  $.ajax({
+    type: 'GET',
+    url: 'https://data.nasa.gov/resource/y77d-th95.json',
+    beforeSend: function (request) {
+      request.setRequestHeader("X-App-Token", token);
+    }
+  }).done(function(response) {
+    $.each(response, function(prop, obj) {
+      m_fell_found.push(obj.fall);
+      m_mass_g.push(parseInt(obj.mass));
+      m_name.push(obj.name);
+      m_latitude.push(obj.reclat);
+      m_longitude.push(obj.reclong);
+      if (typeof obj.year !== "undefined") {
+        m_year.push(parseInt(obj["year"].substr(0, 4)));
+      } else {
+        m_year.push("N/A");
       }
     }); // end each
 
-    var num_of_meteorites = m_name.length;
-    var meteorites = new Array();
     // populate meteorites array with corresponding values
+    var num_of_meteorites = m_name.length;
     for (var i = 0; i < num_of_meteorites; i++) {
       meteorites.push({
         name:       m_name[i],
@@ -48,20 +51,21 @@ $(document).ready(function(){
         year:       m_year[i],
         latitude:   m_latitude[i],
         longitude:  m_longitude[i],
-        radius: 0.5
+        radius: 1.5
       });
     }
 
     map.bubbles(meteorites, {
+      borderColor: '#D85332',
+      borderWidth: 1.2,
       popupTemplate: function (geo, data) {
-              return ['<div class="hoverinfo">' +  data.name,
-              '<br/>Mass (g): ' +  data.mass_g,
-              '<br/>Fell/Found: ' +  data.fell_found + '',
-              '<br/>Year: ' +  data.year + '',
-              '</div>'].join('');
+              return [
+                '<div class="hoverinfo"><strong>' +  data.name,
+                '</strong><br/>Mass (g): '        +  data.mass_g,
+                '<br/>Fell/Found: '               +  data.fell_found + '',
+                '<br/>Year: '                     +  data.year + '',
+                '</div>'].join('');
       }
     }); // end bubbles
-
-
-  }); // end getJSON
+  }); // end done
 }); // end ready
